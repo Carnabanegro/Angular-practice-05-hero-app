b6bf0c10a15ec251d2cc67e419d16477ed141d12
Patricio Serra <pserra@oliver.local>
2022-09-22 16:49:13 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Comp404Component } from './shared/comp404/comp404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //FORMA DE CARGAS UN MODULO CON LAS RUTAS HIJAS EN UNA RUTA PRINCIPAL//
+  {
+    path:'auth', 
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path:'heroes', 
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  //
+  {path:'404', component: Comp404Component},
+  {path:'**', redirectTo: '404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Comp404Component } from './shared/comp404/comp404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp404Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
